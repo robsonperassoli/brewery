e01cf9509b79423ede34acd75a4a19e584cb894d
Robson Gian Perassoli <robsonperassoli@gmail.com>
2020-02-27 23:35:35 -0300

    style batch step list

--- lib/brewery_web/views/batches.ex
+++ lib/brewery_web/views/batches.ex
@@ -1,3 +1,12 @@
 defmodule BreweryWeb.BatchesView do
   use BreweryWeb, :view
+
+  def format_step("fermentation_start"), do: "Fermentation Start"
+  def format_step("dry_hopping"), do: "Dry-Hopping"
+  def format_step("maturation_start"), do: "Maturation Start"
+  def format_step("bottling"), do: "Bottling"
+
+  def format_date(date) do
+    "#{date.month}/#{date.day}/#{date.year}"
+  end
 end
