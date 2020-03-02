7778c31bae13de892c080e1c36a8208c8aa9d737
Robson Gian Perassoli <robsonperassoli@gmail.com>
2020-03-02 14:31:02 -0300

    diferentiate selected link on nav

--- lib/brewery_web/views/layout_view.ex
+++ lib/brewery_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule BreweryWeb.LayoutView do
   use BreweryWeb, :view
+
+  @active_class "is-active"
+
+  def nav_active_class(%{request_path: "/batches" <> _rest}, "/batches"), do: @active_class
+  def nav_active_class(%{request_path: "/styles" <> _rest}, "/styles"), do: @active_class
+  def nav_active_class(%{request_path: "/calculator/priming"}, "/calculator/priming"), do: @active_class
+  def nav_active_class(_, _), do: ""
 end
