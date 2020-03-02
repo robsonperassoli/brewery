4c11aa2b28be35ca02863209e0c1e4e2ada0c9e7
Robson Gian Perassoli <robsonperassoli@gmail.com>
2020-03-02 12:08:19 -0300

    add bottle variations to priming calulator

--- lib/brewery/priming.ex
+++ lib/brewery/priming.ex
@@ -1,5 +1,6 @@
 defmodule Atomic.Priming do
   @ml_per_gram 1.71
+  @bottle_sizes [300, 500, 600]
 
   def grams_per_bottle(size_ml, grams_per_liter), do: (size_ml * grams_per_liter) / 1000
 
@@ -14,7 +15,23 @@ defmodule Atomic.Priming do
       priming_solution: round(priming_solution),
       total_sugar: round(total_sugar),
       solution_per_bottle: Float.round(solution_per_bottle, 1),
-      sugar_per_bottle: sugar_per_bottle
+      sugar_per_bottle: sugar_per_bottle,
+      bottle_variants: bottle_variants(bottle_size, solution_per_bottle)
     }
+  end
+
+
+  def variant_solution(bottle_size, solution_per_bottle, variant_size) do
+    variant_solution = (variant_size * solution_per_bottle) / bottle_size
+
+    %{size: variant_size, solution: Float.round(variant_solution, 1)}
+  end
+
+  def bottle_variants(bottle_size, solution_per_bottle) do
+    calculator = fn variant_size -> variant_solution(bottle_size, solution_per_bottle, variant_size) end
+
+    @bottle_sizes
+    |> Enum.filter(fn size -> size != bottle_size end)
+    |> Enum.map(calculator)
   end
 end
